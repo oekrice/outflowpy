05a2bf2b91d4432c6e6e72c482f0cf7aeb363df9
oekrice <oliverrrice1998@gmail.com>
2025-10-29 16:25:11 +0000

    Improved accuracy of astronomical constants

--- outflowpy/input.py
+++ outflowpy/input.py
@@ -64,9 +64,10 @@ class Input:
         self._grid = Grid(ns, nphi, nr, rss)
 
         #Assuming the solution for the isothermal corona, calculate the sound speed and critical radius etc.
+        #Could import these from astopy.constants but I don't think they're going to change any time soon, so I'll assume it's fine
         mf_in_sensible_units = mf_constant*(6.957e10)**2   #In seconds/solar radius
-        sound_speed = np.sqrt(1.380649e-23*corona_temp/1.67262192e-27) #Sound speed in m/s
-        self.r_c = (6.6743e-11*1.989e30/(2*sound_speed**2))/(6.957e8)   #Critical radius in solar radii (code units)
+        sound_speed = np.sqrt(1.38064852e-23*corona_temp/1.67262192e-27) #Sound speed in m/s
+        self.r_c = (6.67408e-11*1.98847542e30/(2*sound_speed**2))/(6.957e8)   #Critical radius in solar radii (code units)
         self.c_s = mf_in_sensible_units*sound_speed/6.957e8  #Sound speed in seconds/solar radius (code units)
 
         self.vg, self.vcx, self.vdg = self._get_parker_wind_speed()
