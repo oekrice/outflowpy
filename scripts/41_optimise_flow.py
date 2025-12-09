Fixed edge detection scaling issue

scripts/41_optimise_flow.py
@@ -99,8 +99,9 @@ def find_eclipse_flines(eclipse_year, field_parameters):
         coords.representation_type = 'cartesian'
         line = np.zeros((2, len(coords)))
         line[0,:] = coords.y/const.R_sun; line[1,:] = coords.z/const.R_sun
+
         transformed_lines.append([line[0,:], line[1,:]])
-        #For every point along the line, log the position (do angle from the top, clockwise?. Maybe just arctan2 is best) and the angle
+
         for i in range(1,len(line[0])-1):
             x = line[0][i]; y = line[1][i]
             angle = np.arctan2(np.abs(y), np.abs(x))
@@ -199,8 +200,8 @@ def run_optimisation():
     print('Doing optimisation run on eclipse', eclipse_year)
     if os.path.exists("batch_logs/log_%d.txt" % eclipse_year):
         os.remove("batch_logs/log_%d.txt" % eclipse_year)
-    initial_parameter_set = np.array([5.0,1.5])
-    es = cma.CMAEvolutionStrategy(initial_parameter_set, 0.5, {'verb_disp': 1})
+    initial_parameter_set = np.array([5.0,2.5])
+    es = cma.CMAEvolutionStrategy(initial_parameter_set, 0.25, {'verb_disp': 1})
     es.optimize(generate_fn)
     es.result_pretty()
 

scripts/edge_detection.py
@@ -42,9 +42,6 @@ def make_angle_histogram(flines, bin_resolution = 10, num = 0, resolution = 512)
         #Given coordinates x, y, determine the radius in suns
         return np.sqrt(x**2 + y**2)
 
-    def pt_to_xy(pt):
-        return 5.0*(pt[0] - resolution/2)/resolution, -5.0*(pt[1] - resolution/2)/resolution
-
     histogram_sum = np.zeros((nbins_r, nbins_theta))
     histogram_count = np.zeros((nbins_r, nbins_theta))
     xs, ys, cs = [], [], []
@@ -56,13 +53,10 @@ def make_angle_histogram(flines, bin_resolution = 10, num = 0, resolution = 512)
             dx = line[0][i+1] - line[0][i-1]
             dy = line[1][i+1] - line[1][i-1]
 
-
-
-            x, y = pt_to_xy([line[0][i], line[1][i]])
             #x = line[0][i] - resolution/2; y = -1.0*(line[1][i] - resolution/2)
             angle = np.arctan2(y, x)
-            x_up, y_up     = pt_to_xy([line[0][i+1], line[1][i+1]])
-            x_down, y_down = pt_to_xy([line[0][i-1], line[1][i-1]])
+            x_up, y_up     = line[0][i+1], line[1][i+1]
+            x_down, y_down = line[0][i-1], line[1][i-1]
 
             dx = x_up - x_down
             dy = y_up - y_down