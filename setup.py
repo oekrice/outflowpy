dea88fcdc1c01336d77cfa7aae80a4c1a99956c3
Oliver on Maths Linux <vgjn10@airy.durham.ac.uk>
2026-01-05 16:50:26 +0000

    Fix build

--- setup.py
+++ setup.py
@@ -1,79 +1,45 @@
 import subprocess
-import sys
 from pathlib import Path
 from setuptools import setup, Extension
 from setuptools.command.build_py import build_py
 
-# class F2PyBuild(build_py):
-#     def run(self):
-#
-#         import numpy
-#
-#         build_dir = Path(self.build_lib) / "outflowpy"
-#         build_dir.mkdir(parents=True, exist_ok=True)
-#         # Attempt to compile the fotran code as per
-#         subprocess.check_call([
-#             "python", "-m", "numpy.f2py",
-#             "-c", "fortran/outflow_calc.f90",
-#             "-m", "outflow_calc"
-#         ])
-#
-#         subprocess.check_call([
-#             "python", "-m", "numpy.f2py",
-#             "-c", "fortran/fast_tracer.f90",
-#             "-m", "fast_tracer", "--f90flags='-fopenmp'", "-lgomp"
-#         ])
-#
-#         for file in Path(".").glob("outflow_calc*.so"):
-#             print('File found and moving', build_dir / file.name)
-#             file.rename(build_dir / file.name)
-#         for file in Path(".").glob("outflow_calc*.pyd"):
-#             file.rename(build_dir / file.name)
-#         for file in Path(".").glob("outflow_calc*.c"):
-#             file.rename(build_dir / file.name)
-#
-#         for file in Path(".").glob("fast_tracer*.so"):
-#             print('File found and moving', build_dir / file.name)
-#             file.rename(build_dir / file.name)
-#         for file in Path(".").glob("fast_tracer*.pyd"):
-#             file.rename(build_dir / file.name)
-#         for file in Path(".").glob("fast_tracer*.c"):
-#             file.rename(build_dir / file.name)
-#
-#         super().run()
+class F2PyBuild(build_py):
+    def run(self):
 
+        import numpy
 
-omp_compile = []
-omp_link = []
+        build_dir = Path(self.build_lib) / "outflowpy"
+        build_dir.mkdir(parents=True, exist_ok=True)
+        # Attempt to compile the fotran code as per
+        subprocess.check_call([
+            "python", "-m", "numpy.f2py",
+            "-c", "fortran/outflow_calc.f90",
+            "-m", "outflow_calc"
+        ])
 
-if sys.platform.startswith("linux"):
-    omp_compile = ["-fopenmp"]
-    omp_link = ["-fopenmp"]
-elif sys.platform == "darwin":
-    omp_compile = ["-Xpreprocessor", "-fopenmp"]
-    omp_link = ["-lomp"]
-elif sys.platform == "win32":
-    omp_compile = ["/openmp"]
+        subprocess.check_call([
+            "python", "-m", "numpy.f2py",
+            "-c", "fortran/fast_tracer.f90",
+            "-m", "fast_tracer", "--f90flags='-fopenmp'", "-lgomp"
+        ])
 
-Extension(
-    name="outflowpy.fast_tracer",
-    sources=["fortran/fast_tracer.f90"],
-    extra_f90_compile_args=omp_compile,
-    extra_link_args=omp_link,
-)
+        for file in Path(".").glob("outflow_calc*.so"):
+            print('File found and moving', build_dir / file.name)
+            file.rename(build_dir / file.name)
+        for file in Path(".").glob("outflow_calc*.pyd"):
+            file.rename(build_dir / file.name)
+        for file in Path(".").glob("outflow_calc*.c"):
+            file.rename(build_dir / file.name)
 
-ext_modules = [
-    Extension(
-        name="outflowpy.outflow_calc",
-        sources=["fortran/outflow_calc.f90"],
-    ),
-    Extension(
-        name="outflowpy.fast_tracer",
-        sources=["fortran/fast_tracer.f90"],
-        extra_f90_compile_args=["-fopenmp"],
-        extra_link_args=["-fopenmp"],
-    ),
-]
+        for file in Path(".").glob("fast_tracer*.so"):
+            print('File found and moving', build_dir / file.name)
+            file.rename(build_dir / file.name)
+        for file in Path(".").glob("fast_tracer*.pyd"):
+            file.rename(build_dir / file.name)
+        for file in Path(".").glob("fast_tracer*.c"):
+            file.rename(build_dir / file.name)
+
+        super().run()
 
 setup(
     name="outflowpy",
@@ -82,6 +48,5 @@ setup(
     author="Oliver Rice",
     author_email="oliverricesolar@gmail.com",
     packages=["outflowpy"],
-    ext_module = ext_modules,
-    #cmdclass={"build_py": F2PyBuild},
+    cmdclass={"build_py": F2PyBuild},
 )
