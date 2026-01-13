a50d74db584556f0a98f3cc8daa4910b2e421cc1
Oliver on Maths Linux <vgjn10@airy.durham.ac.uk>
2026-01-13 15:19:22 +0000

    Compiling meson.build state but has copying error

--- outflowpy/__init__.py
+++ outflowpy/__init__.py
@@ -31,19 +31,3 @@ from ._version import get_versions
 
 __version__ = get_versions()['version']
 del get_versions
-
-
-__citation__ = __bibtex__ = """
-@article{Stansby2020,
-doi = {10.21105/joss.02732},
-url = {https://doi.org/10.21105/joss.02732},
-year = {2020},
-publisher = {The Open Journal},
-volume = {5},
-number = {54},
-pages = {2732},
-author = {David Stansby and Anthony Yeates and Samuel T. Badman},
-title = {pfsspy: A Python package for potential field source surface modelling},
-journal = {Journal of Open Source Software}
-}
-"""
